7beded138752abf593d63be5d34da15a6b65cafe
castamor <castamorr@gmail.com>
2025-04-26 00:28:16 -0700

    Configuracion inicial del proyecto

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Rubik } from 'next/font/google';
+import "./globals.css";
+
+export const rubik = Rubik({
+    subsets: ['latin'],
+    variable: '--font-rubik',
+    display: 'swap',
+});
+
+export const metadata: Metadata = {
+    title: "Preguntas Quizz",
+    description: "Juego de preguntas y respuestas de varios temas",
+    icons: {
+        icon: "/favicon.svg",
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="es">
+            <body
+                className={`${rubik.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-rubik)]">
+            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+            </main>
+        </div>
+    );
+}
