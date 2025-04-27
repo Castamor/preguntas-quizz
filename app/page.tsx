724e81ee2d182756e7a4bf9008474c276054859c
castamor <castamorr@gmail.com>
2025-04-26 19:41:18 -0700

    Primer version del menú principal

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,24 @@
 export default function Home() {
+
+    const TEMAS = ['Programación', 'Videojuegos', 'Astrología', 'Geografía'];
+
     return (
-        <div className="grid grid-rows-[0px_1fr_0px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <div className="contenido grid grid-rows-[0px_1fr_0px] items-center min-h-screen gap-16 mx-auto">
+            <main className="flex flex-col gap-[32px] row-start-2 ">
+                <div>
+                    <h1 className="font-bold text-3xl text-center text-amber-300 uppercase">Preguntas Quizz</h1>
+
+                    <p className="text-xl font-bold mt-6">Vamos a jugar</p>
+                    <p className="">Elige un tema y prueba tus capacidades.</p>
+                </div>
+
+                <ul className="grid grid-cols-2 grid-rows-2 text-center gap-4 aspect-square max-w-[20rem] mx-auto">
+                    {TEMAS.map(tema => (
+                        <li key={tema} className="border flex items-center justify-center font-semibold uppercase cursor-pointer rounded-xl p-8">
+                            {tema}
+                        </li>
+                    ))}
+                </ul>
             </main>
         </div>
     );
