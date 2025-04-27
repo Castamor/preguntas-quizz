d15b4e82a367222a554453d116d353225b0c369a
castamor <castamorr@gmail.com>
2025-04-26 19:03:56 -0700

    Ajustar fuente de Rubik

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     return (
         <html lang="es">
             <body
-                className={`${rubik.variable} antialiased`}
+                className={`${rubik.className} antialiased `}
             >
                 {children}
             </body>
