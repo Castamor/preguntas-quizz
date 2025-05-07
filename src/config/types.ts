01bd9b0958cd835a526e8a4e33b841b4a276a942
castamor <castamorr@gmail.com>
2025-05-06 19:39:42 -0700

    Moficiar tipos de las preguntas

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,5 +1,3 @@
-export type TEMAS = 'programacion' | 'videojuegos' | 'astrologia' | 'geografia';
-
 export interface Respuesta {
    id: string;
    texto: string;
@@ -7,7 +5,6 @@ export interface Respuesta {
 
 export interface Pregunta {
    id: string;
-   tema: TEMAS;
    enunciado: string;
    respuestas: Respuesta[];
    idRespuestaCorrecta: string;
