3459062a389660b37acfd810c33db3ba6d25e265
castamor <castamorr@gmail.com>
2025-05-09 21:19:58 -0700

    Modificar ItemTema para usar un botón en lugar de un elemento de lista y ajustar la función de manejo de clics

--- src/components/UI/ItemTema.tsx
+++ src/components/UI/ItemTema.tsx
@@ -1,6 +1,6 @@
 "use client"
 import './ItemTema.css'
-import { eliminarClasesActive } from '@/config/utils'
+import { handleClaseActive } from '@/config/utils'
 
 interface ItemTemaProps {
    tema: string
@@ -9,21 +9,21 @@ interface ItemTemaProps {
 
 export default function ItemTema({ tema, children }: ItemTemaProps) {
 
-   function handleClick(e: React.MouseEvent<HTMLLIElement>) {
-      if (e.currentTarget.classList.contains('active')) return
+   function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
+      handleClaseActive(e, '.item')
 
-      eliminarClasesActive('.item')
-      e.currentTarget.classList.add('active')
       console.log(`guardando el tema: '${tema}' en localStorage`)
    }
 
    return (
-      <li
+      <button
+         type="button"
+         title={tema}
          className="item cursor-pointer"
          onClick={handleClick}
       >
          <div className="bg-neutral-100 aspect-square rounded-xl">{children}</div>
          <p>{tema}</p>
-      </li>
+      </button>
    )
 }
