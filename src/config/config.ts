acbf6173c3a93bb06b0824413edfec8a2ae60a40
castamor <castamorr@gmail.com>
2025-05-09 21:20:30 -0700

    Agregar archivo de configuración con las dificultades y temas disponibles

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const DIFICULTADES = {
+   normal: {
+      texto: 'normal',
+      tiempo_por_pregunta: 20,
+      total_preguntas: 10,
+   },
+   dificil:{
+      texto: 'dificil',
+      tiempo_por_pregunta: 10,
+      total_preguntas: 20,
+   },
+}
+
+export const TEMAS = {
+   programacion: { texto: 'programación' },
+   videojuegos: { texto: 'videojuegos' },
+   entretenimiento: { texto: 'entretenimiento' },
+   astronomía: { texto: 'astronomía' }
+}
