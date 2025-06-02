4920eaf77629081619e76bc9bd414d418a932b34
castamor <castamorr@gmail.com>
2025-06-02 14:52:06 -0700

    Refactorizar componente Jugar para utilizar Link en lugar de botón y mejorar la lógica de habilitación del juego

--- src/components/Jugar.tsx
+++ src/components/Jugar.tsx
@@ -1,6 +1,43 @@
+"use client"
+
+import { useJuegoStore } from "@/store/juego"
+import { useEffect, useState } from "react"
+import Link from "next/link"
+import { NADA } from "@/config"
+
 export default function Jugar() {
+
+   const temaStore = useJuegoStore(state => state.tema)
+   const dificultadStore = useJuegoStore(state => state.dificultad)
+   const [sePuedeJugar, setSePuedeJugar] = useState(false)
+
+   const hayTema = temaStore !== NADA
+   const hayDificultad = dificultadStore !== NADA
+
+   useEffect(() => {
+      setSePuedeJugar(hayTema && hayDificultad)
+   }, [hayTema, hayDificultad])
+
+   function handleClick(e: React.MouseEvent<HTMLAnchorElement>) {
+      if (!sePuedeJugar) {
+         e.preventDefault()
+         return
+      }
+
+   }
+
    return (
-      <button type="button" className=" py-2 px-4 bg-blue-500 rounded-2xl cursor-pointer hover:bg-blue-400 transition-colors duration-75 mx-auto">Jugar</button>
+      <Link
+         type="button"
+         href={`/juego/${temaStore}`}
+         className={`
+            btn-juego py-2 px-4 mx-auto rounded-2xl transition-colors duration-75
+            ${sePuedeJugar ? "bg-blue-500 cursor-pointer" : "bg-blue-400 cursor-not-allowed"}
+         `}
+         onClick={handleClick}
+      >
+         Jugar
+      </Link>
    )
 
 }
