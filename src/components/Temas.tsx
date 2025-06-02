54cb5f46226ce3d8196a9bff0ee903e3e0acc85a
castamor <castamorr@gmail.com>
2025-06-02 13:57:17 -0700

    Refactorizar componentes ItemTema y Temas para mejorar la gestión de temas y corregir estilos

--- src/components/Temas.tsx
+++ src/components/Temas.tsx
@@ -1,14 +1,14 @@
-import { TEMAS } from '@/config/config'
+import { TEMAS } from '@/config'
 import ItemTema from './UI/ItemTema'
 
 export default function Temas() {
 
    return (
       <ul className="grid grid-cols-2 grid-rows-2 text-center gap-4 max-w-[20rem] mx-auto">
-         <ItemTema tema={TEMAS.programacion.texto}></ItemTema>
-         <ItemTema tema={TEMAS.videojuegos.texto}></ItemTema>
-         <ItemTema tema={TEMAS.entretenimiento.texto}></ItemTema>
-         <ItemTema tema={TEMAS.astronomía.texto}></ItemTema>
+         <ItemTema tema={TEMAS.programacion}></ItemTema>
+         <ItemTema tema={TEMAS.videojuegos}></ItemTema>
+         <ItemTema tema={TEMAS.entretenimiento}></ItemTema>
+         <ItemTema tema={TEMAS.astronomía}></ItemTema>
       </ul >
    )
 
--- src/components/UI/ItemTema.tsx
+++ src/components/UI/ItemTema.tsx
@@ -1,34 +1,46 @@
 "use client"
-import { handleClaseActive } from '@/config/utils'
+import { Tema as TemaType } from '@/config/types'
+import { PropsWithChildren, useEffect, useState } from 'react'
 import { useJuegoStore } from '@/store/juego'
+import { handleClasesActives } from '@/helpers'
+import { NADA } from '@/config'
 import './ItemTema.css'
 
-interface ItemTemaProps {
-   tema: string
-   children?: React.ReactNode
+interface ItemTemaProps extends PropsWithChildren {
+   tema: TemaType
 }
 
 export default function ItemTema({ tema, children }: ItemTemaProps) {
 
-   const temaStore = useJuegoStore((state) => state.tema)
-   const setTema = useJuegoStore((state) => state.setTema)
+   const temaStore = useJuegoStore(state => state.tema)
+   const setTemaStore = useJuegoStore(state => state.setTema)
+   const [estaActivo, setEstaActivo] = useState(false)
+
+   useEffect(() => {
+      handleClasesActives('.itemTema')
+      setEstaActivo(temaStore === tema.slug)
+   }, [tema.slug, temaStore])
 
    function handleClick(e: React.MouseEvent<HTMLButtonElement>) {
-      const activo = handleClaseActive(e, '.item')
-      if (activo) return
+      const item = e.currentTarget.classList
 
-      setTema(tema)
+      if (item.contains('active')) {
+         item.toggle('active')
+         setTemaStore(NADA)
+         return
+      }
+      setTemaStore(tema.slug)
    }
 
    return (
       <button
          type="button"
-         title={tema}
-         className={`item ${(temaStore === tema ? 'active' : '')} cursor-pointer`}
+         title={tema.texto}
+         className={`itemTema ${tema.slug} ${estaActivo ? 'active' : ''} cursor-pointer`}
          onClick={handleClick}
       >
          <div className="bg-neutral-100 aspect-square rounded-2xl">{children}</div>
-         <p className='capitalize'>{tema}</p>
+         <p className='capitalize'>{tema.texto}</p>
       </button >
    )
 }
